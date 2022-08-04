9ec3fb9791242f465ba954cdc4679ac245f2bcce
DDkhoa2k8 <110537606+DDkhoa2k8@users.noreply.github.com>
2022-08-04 15:04:43 +0700

    Update AIlib.js

--- AIlib.js
+++ AIlib.js
@@ -163,19 +163,77 @@ function weiGen(lt) {
     return wei;
 }
 ///
-function ANN(lr,mo) {
+function ANN(lr,mo,act) {
 	this.s = 0;
 	this.wGS;
     this.weight = [];
 	this.nodeData = [];
 	this.netData = [];
+	this.propageGPU;
+	this.actGPU;
+	this.canvas = [];
 	if (LIB.gpuMode) {
 		this.wGS = checkWebGL();
 		if (this.wGS[0]) {
-			s = this.wGS[1];
+			this.s = this.wGS[1];
 		}
 	}
-	console.log(this.s);
+	this.PropageWebGL1 = () => {
+		let canvas = document.createElement("canvas");
+		let gl = canvas.getContext("webgl");
+		var ver = `
+			attribute vec4 position;
+			void main() {
+				gl_Position = position;
+			}`,
+		frag = `
+			precision highp float;
+			precision highp int;
+			vec4 encode_float(float v) {
+				if (v == 0.0) { 
+					return vec4(0.0,0.0,0.0,0.0);
+				} 
+				float a = abs(v),
+				exp = floor(log2(a)),
+				mant = (a * pow(2.0,23.0 - exp)),
+				mant1 = floor(mant / 256.0 / 256.0),
+				mant2 = mod(floor(mant / 256.0),256.0),
+				mant3 = mod(mant,256.0),
+				sign = 128.0-128.0 * (a / v),
+				e = (sign + exp + 127.0) / 510.0,
+				m1 = (mant1 - (128.0 * (1.0 - mod(exp + 127.0,2.0)))) / 255.0,
+				m2 = (mant2) / 255.0,
+				m3 = (mant3 + 0.5) / 255.0;
+				return vec4(m3,m2,m1,e); 
+			}
+			float decode_float(vec4 v) {
+				vec4 bits = v * 255.0;
+				float sign = mix(-1.0, 1.0, step(bits[3], 128.0)),
+				expo = floor(mod(bits[3] + 0.1, 128.0)) * 2.0 + floor((bits[2] + 0.1) / 128.0) - 127.0,
+				sig = bits[0] + bits[1] * 256.0 + floor(mod(bits[2] + 0.1, 128.0)) * 256.0 * 256.0;
+				return sign * (1.0 + sig / 8388607.0) * pow(2.0, expo);
+			}
+			void main() {
+				
+			}
+			gl_FragColor = encode_float(sum);
+		}`;
+		var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+		gl.shaderSource(fragmentShader, frag);
+		gl.compileShader(fragmentShader);
+		var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+		gl.shaderSource(vertexShader, ver);
+		gl.compileShader(vertexShader);
+		program = gl.createProgram();
+		gl.attachShader(program, vertexShader);
+		gl.attachShader(program, fragmentShader);
+		gl.linkProgram(program);
+		gl.useProgram(program);
+		return [gl,canvas];
+	}
+	this.PropageWebGL2 = () => {
+		
+	}
     this.setup = (build) => {
         //s loop
 		let wei;
@@ -209,25 +267,30 @@ function ANN(lr,mo) {
 		this.nodeData.push(output);
 		return output;
 	}
+	this.getContextWebGL1 = (act) => {
+		this.propageGPU = this.PropageWebGL1();
+		this.actGPU = window[act + "WebGL1"]();
+	}
+	this.getContextWebGL2 = (act) => {
+		this.propageGPU = this.PropageWebGL2();
+		this.actGPU = window[act + "WebGL2"]();
+	}
     this.propagation = (input) => {
+		var webGL;
 		if (this.s == 0) {
 			let netIn = input;
 			for (let l = 0,len = this.weight.length;l < len;l++) {
-				netIn = this.Perceptron(netIn,this.weight[l],"tanh",0);
+				netIn = this.Perceptron(netIn,this.weight[l],"reLU",0);
 			}
 			return  netIn;
-		} else if (this.s == 1) {
-			let propageGPU
-			,actGPU;
-			propageGPU = this.PropageWebGL1();
-			actGPU = window[act + "WebGL1"]();
+		} else if (this.s == 3) {
+			this.getContextWebGL1(act);
 		} else {
-			let propageGPU
-			,actGPU;
-			propageGPU = this.PropageWebGL2();
-			actGPU = window[act + "WebGL2"]();
-			console.log(1);
+			this.getContextWebGL2(act);
 		}
+		return (input) => {
+			var _that = this;
+		};
     }
 	this.backpropagation = (out,pOut) => {
 
